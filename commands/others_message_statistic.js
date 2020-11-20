a78806b725cdc5d0c3a6a0e935c9e4c9c2adb56e
cjh980402 <9804cjh@naver.com>
2020-11-20 12:04:29 +0900

    채팅량 기능 수정

--- commands/others_message_statistic.js
+++ commands/others_message_statistic.js
@@ -1,23 +1,25 @@
-function guildNickname(guild, user) {
-    return guild.members.cache.find(v => v.user == user).nickname || user.username;
-}
-
 module.exports = {
-    usage: `${client.prefix}채팅량`,
+    usage: `${client.prefix}채팅량 @대상유저멘션`,
     command: ["채팅량", "ㅊㅌㄹ"],
-    description: "- 본인의 채팅 통계를 보여줍니다.",
+    description: "- 해당하는 유저의 채팅 통계를 보여줍니다. 대상유저를 생략시에는 본인의 채팅 통계를 보여줍니다.",
     channelCool: true,
     type: ["기타"],
-    async execute(message) {
-        const messagestat = await db.get(`SELECT * FROM messagedb WHERE channelsenderid = ?`, [`${message.guild.id} ${message.author.id}`]);
+    async execute(message, args) {
+        if (!message.guild) {
+            return message.channel.send("사용이 불가능한 채널입니다.");
+        }
+        const findId = /<@!?(\d+)>/.exec(args[0]);
+        const targetId = findId ? findId[1] : message.author.id;
+
+        const messagestat = await db.get(`SELECT * FROM messagedb WHERE channelsenderid = ?`, [`${message.guild.id} ${targetId}`]);
         if (messagestat) {
-            return message.channel.send(`${guildNickname(message.guild, message.author)}
+            return message.channel.send(`<@${targetId}>
 채팅 건수: ${messagestat.messagecnt}
 문자 개수: ${messagestat.lettercnt}
 채팅 지수: ${(messagestat.lettercnt / messagestat.messagecnt).toFixed(2)}`);
         }
         else {
-            return message.channel.send(`${guildNickname(message.guild, message.author)}\n채팅 건수: 0\n문자 개수: 0\n채팅 지수: 0.00`);
+            return message.channel.send(`<@${targetId}>\n채팅 건수: 0\n문자 개수: 0\n채팅 지수: 0.00`);
         }
     }
 };
