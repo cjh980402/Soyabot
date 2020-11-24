e23cb587b99d952c0035c1acc679b3d5884d9a79
cjh980402 <9804cjh@naver.com>
2020-11-24 17:40:50 +0900

    봇 상태 명령어 추가

---
+++ commands/others_bot_status.js
@@ -0,0 +1,21 @@
+const OS = require('os');
+
+module.exports = {
+    usage: `${client.prefix}상태`,
+    command: ["상태", "ㅅㅌ"],
+    description: "소야봇의 작동 상태를 알려줍니다.",
+    type: ["기타"],
+    async execute(message) {
+        let seconds = Math.floor(client.uptime / 1000);
+        let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
+        let days = Math.floor(hours / 24);
+        seconds %= 60;
+        minutes %= 60;
+        hours %= 24;
+
+        const memory = 100 - Math.round(OS.freemem() / OS.totalmem() * 100);
+
+        return message.reply(`작동 시간: ${days}일 ${hours}시간 ${minutes}분 ${seconds}초\n메모리 사용량: ${memory}%`);
+    }
+};
