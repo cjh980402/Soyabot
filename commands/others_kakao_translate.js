fd664eabfe40ff80f79fd3d4124529d9f316064f
cjh980402 <9804cjh@naver.com>
2020-12-11 22:56:53 +0900

    카카오 번역기능 추가

---
+++ commands/others_kakao_translate.js
@@ -0,0 +1,76 @@
+const { KAKAO_API_KEY } = require('../soyabot_config.json');
+const fetch = require('node-fetch');
+const langList = {
+    "한": ["한국어", "kr"],
+    "영": ["영어", "en"],
+    "일": ["일본어", "jp"],
+    "중": ["중국어", "cn"],
+    "베": ["베트남어", "vi"],
+    "인": ["인도네시아어", "id"],
+    "아": ["아랍어", "ar"],
+    "뱅": ["뱅갈어", "bn"],
+    "독": ["독일어", "de"],
+    "스": ["스페인어", "es"],
+    "프": ["프랑스어", "fr"],
+    "힌": ["힌디어", "hi"],
+    "이": ["이탈리아어", "it"],
+    "말": ["말레이시아어", "ms"],
+    "네": ["네덜란드어", "nl"],
+    "포": ["포르투갈어", "pt"],
+    "러": ["러시아어", "ru"],
+    "태": ["태국어", "th"],
+    "터": ["터키어", "tr"],
+};
+
+async function tran(source, target, text) {
+    const params = new URLSearchParams();
+    params.append("src_lang", source);
+    params.append("target_lang", target);
+    params.append("query", text);
+    const response = await fetch("https://dapi.kakao.com/v2/translation/translate", {
+        method: 'POST',
+        headers: {
+            "Authorization": `KakaoAK ${KAKAO_API_KEY}`
+        },
+        body: params
+    });
+    const data = await response.json();
+    return data.translated_text?.join("\n") ?? "번역에 실패하였습니다.";
+}
+
+function findLangCode(src, tar) {
+    if (langList[src] && langList[tar]) {
+        return [langList[src][1], langList[tar][1]]
+    }
+    else {
+        return null;
+    }
+}
+
+module.exports = {
+    usage: `${client.prefix}번역 (대상 언어 첫글자 + 결과 언어 첫글자) (내용)`,
+    command: ["번역", "ㅂㅇ"],
+    description: `- 카카오 i를 이용한 언어 번역을 수행합니다. 한국어 → 영어의 경우 두번째 매개변수는 한영으로 입력하면 됩니다.
+- 참고. ${client.prefix}번역 목록`,
+    type: ["기타"],
+    async execute(message, args) {
+        if (args[0] == '목록' || args[0] == 'ㅁㄹ') {
+            return message.channel.send(`<지원하는 언어 종류>\n${Object.values(langList).map(v => v[0]).join(", ")}`);
+        }
+        if (args.length < 2) {
+            return message.channel.send(`${this.usage}\n- 대체 명령어: ${this.command.join(', ')}\n${this.description}`);
+        }
+        const langCode = findLangCode(args[0][0], args[0][1]);
+        if (langCode == null) {
+            return message.channel.send(`형식에 맞지 않거나 지원하지 않는 번역입니다.\n입력형식은 "${this.usage}"입니다.\n언어의 목록은 ${client.prefix}번역 목록을 확인해주세요.`);
+        }
+
+        const msg = args.slice(1).join(' ');
+        if (msg.length > 5000) {
+            return message.channel.send('5000자를 초과하는 내용은 번역하지 않습니다.');
+        }
+        else {
+            return message.channel.send(await tran(langCode[0], langCode[1], msg));
+        }
+    }
+};
--- commands/others_papago.js
+++ commands/others_papago.js
@@ -2,24 +2,20 @@ const fetch = require('node-fetch');
 const { clientId, clientSecret } = require("../soyabot_config.json");
 
 async function tran(source, target, text) {
-    try {
-        const params = new URLSearchParams();
-        params.append("source", source);
-        params.append("target", target);
-        params.append("text", text);
-        const response = await fetch("https://openapi.naver.com/v1/papago/n2mt", {
-            method: 'POST',
-            headers: {
-                "X-Naver-Client-Id": clientId,
-                "X-Naver-Client-Secret": clientSecret
-            },
-            body: params
-        });
-        return (await response.json()).message.result.translatedText;
-    }
-    catch (e) {
-        return e; //오류 반환
-    }
+    const params = new URLSearchParams();
+    params.append("source", source);
+    params.append("target", target);
+    params.append("text", text);
+    const response = await fetch("https://openapi.naver.com/v1/papago/n2mt", {
+        method: 'POST',
+        headers: {
+            "X-Naver-Client-Id": clientId,
+            "X-Naver-Client-Secret": clientSecret
+        },
+        body: params
+    });
+    const data = await response.json();
+    return data.message?.result.translatedText ?? "번역에 실패하였습니다.";
 }
 
 function checklan(src, tar) {
@@ -38,28 +34,27 @@ function checklan(src, tar) {
 }
 
 module.exports = {
-    usage: `${client.prefix}번역 (번역 대상 언어) (번역 결과 언어) (내용)`,
-    command: ["번역", "ㅂㅇ"],
+    usage: `${client.prefix}파파고 (번역 대상 언어) (번역 결과 언어) (내용)`,
+    command: ["파파고", "ㅍㅍㄱ"],
     description: `- 파파고를 이용한 언어 번역을 수행합니다.
-- 참고. ${client.prefix}번역 목록`,
+- 참고. ${client.prefix}파파고 목록`,
     type: ["기타"],
     async execute(message, args) {
+        if (args[0] == '목록' || args[0] == 'ㅁㄹ') {
+            const list = '한국어(ko)-영어(en), 한국어(ko)-일본어(ja), 한국어(ko)-중국어 간체(zh-CN), 한국어(ko)-중국어 번체(zh-TW), 한국어(ko)-스페인어(es), 한국어(ko)-프랑스어(fr), 한국어(ko)-러시아어(ru), 한국어(ko)-베트남어(vi), 한국어(ko)-태국어(th), 한국어(ko)-인도네시아어(id), 한국어(ko)-독일어(de), 한국어(ko)-이탈리아어(it), 중국어 간체(zh-CN) - 중국어 번체(zh-TW), 중국어 간체(zh-CN) - 일본어(ja), 중국어 번체(zh-TW) - 일본어(ja), 영어(en)-일본어(ja), 영어(en)-중국어 간체(zh-CN), 영어(en)-중국어 번체(zh-TW), 영어(en)-프랑스어(fr)';
+            return message.channel.send(`<지원하는 번역 종류>\n\n${list.replace(/, /g, '\n')}\n\n순서는 바뀌어도 됩니다.`);
+        }
         if (args.length < 3) {
             return message.channel.send(`**${this.usage}**\n- 대체 명령어: ${this.command.join(', ')}\n${this.description}`);
         }
-        if (args[0] == '목록' || args[0] == 'ㅁㄹ') {
-            const allsee = "\u200b".repeat(500); //강제 장문 전환 목적의 투명문자
-            const list = '한국어(ko)-영어(en), 한국어(ko)-일본어(ja), 한국어(ko)-중국어 간체(zh-CN), 한국어(ko)-중국어 번체(zh-TW), 한국어(ko)-스페인어(es), 한국어(ko)-프랑스어(fr), 한국어(ko)-러시아어(ru), 한국어(ko)-베트남어(vi), 한국어(ko)-태국어(th), 한국어(ko)-인도네시아어(id), 한국어(ko)-독일어(de), 한국어(ko)-이탈리아어(it), 중국어 간체(zh-CN) - 중국어 번체(zh-TW), 중국어 간체(zh-CN) - 일본어(ja), 중국어 번체(zh-TW) - 일본어(ja), 영어(en)-일본어(ja), 영어(en)-중국어 간체(zh-CN), 영어(en)-중국어 번체(zh-TW), 영어(en)-프랑스어(fr)';
-            return message.channel.send(`<지원하는 번역 종류>${allsee}\n\n${list.replace(/, /g, '\n')}\n\n순서는 바뀌어도 됩니다.`);
-        }
 
-        if ((!checklan(args[0], args[1]) && !checklan(args[1], args[0])) || args[2] == undefined) {
-            return message.channel.send(`형식에 맞지 않거나 지원하지 않는 번역입니다.\n입력형식은 "${client.prefix}번역 (대상언어) (결과언어) (번역할 내용)"입니다.\n언어의 형식은 번역 목록을 확인해주세요.`);
+        if (!checklan(args[0], args[1]) && !checklan(args[1], args[0])) {
+            return message.channel.send(`형식에 맞지 않거나 지원하지 않는 번역입니다.\n입력형식은 "${this.usage}"입니다.\n언어의 형식은 ${client.prefix}파파고 목록을 확인해주세요.`);
         }
 
         const msg = args.slice(2).join(' ');
-        if (msg.length > 600) {
-            return message.channel.send('600자를 초과하는 내용은 번역하지 않습니다.');
+        if (msg.length > 1000) {
+            return message.channel.send('1000자를 초과하는 내용은 번역하지 않습니다.');
         }
         else {
             return message.channel.send(await tran(args[0], args[1], msg));
