c1dfea3b75f47bd87f76320ab8500df01a094275
cjh980402 <9804cjh@naver.com>
2020-12-24 22:17:04 +0900

    파이썬 소스 개선

--- util/maple_stats_drawer.py
+++ util/maple_stats_drawer.py
@@ -1,27 +1,20 @@
 from PIL import Image, ImageFont, ImageDraw
-from io import BytesIO
 from random import randrange
 import sys
 
-
 def draw_stats(img: Image, name: str):
-  drawer = ImageDraw.Draw(img)
-  #Draw Name
-  name_coord = (25, 47)
-  drawer.text(name_coord, name, (255, 255, 255),
-              ImageFont.truetype('./fonts/CookieRun Regular.ttf', 14))
+    drawer = ImageDraw.Draw(img)
+    # 이름 쓰기
+    name_coord = (25, 47)
+    drawer.text(name_coord, name, (255, 255, 255), ImageFont.truetype('./fonts/CookieRun Regular.ttf', 14))
 
-  #Draw Stats
-  stats = [4, 4, 4, 4]
-  for i in range(9):
-    stats[randrange(4)] += 1
-  for i, stat in enumerate(stats):
-    stat_coord = (60, 105 + i * 20)
-    drawer.text(stat_coord, str(stat), (0, 0, 0),
-                ImageFont.truetype('./fonts/CookieRun Regular.ttf', 12))
+  # 스탯 배분 후 스탯 쓰기
+    stats = [4, 4, 4, 4]
+    for i in range(9):
+        stats[randrange(4)] += 1
+    for i, stat in enumerate(stats):
+        stat_coord = (60, 105 + i * 20)
+        drawer.text(stat_coord, str(stat), (0, 0, 0), ImageFont.truetype('./fonts/CookieRun Regular.ttf', 12))
+    return img
 
-  return img
-
-
-draw_stats(Image.open('./pictures/character_dice.png'),
-           sys.argv[1]).save('./pictures/dice_result.png')
+draw_stats(Image.open('./pictures/character_dice.png'), sys.argv[1]).save('./pictures/dice_result.png')
