dcea504d4ecbdf37e4f01b4a51fedc7f3b78560b
cjh980402 <9804cjh@naver.com>
2020-12-28 12:19:51 +0900

    공지 보내는 함수 수정

--- admin/bot_control.js
+++ admin/bot_control.js
@@ -3,14 +3,12 @@ const { ADMIN_ID } = require("../soyabot_config.json");
 module.exports.botNotice = async function (data, type = null) {
     const skiplist = type ? (await db.all(`select channelid from ${type}skip`)).map((v) => v.channelid) : [];
     // type이 null(전체 알림)인 경우는 예외리스트는 없음
-    client.guilds.cache.filter((v) => !skiplist.includes(v.id)).forEach((v, i) => {
+    client.guilds.cache.filter((v) => !skiplist.includes(v.id)).forEach((v) => {
         const guildText = v.channels.cache.filter((v) => v.type == 'text');
         const target = guildText.find((v) => v.name.includes("알림")) ?? guildText.first();
-        setTimeout(() => {
-            if (target?.permissionsFor(client.user).has("VIEW_CHANNEL") && target?.permissionsFor(client.user).has("SEND_MESSAGES")) {
-                target.send(data);
-            }
-        }, 1000 * i); // 1000*i ms 이후에 주어진 함수 실행
+        if (target?.permissionsFor(client.user).has("VIEW_CHANNEL") && target?.permissionsFor(client.user).has("SEND_MESSAGES")) {
+            target.send(data); // 디스코드 봇은 딜레이 없이 공지 보내기 가능
+        }
     });
 }
 
