693dfdee62162c16f766cdfde454bafd5ae6fadd
cjh980402 <9804cjh@naver.com>
2021-03-30 11:02:43 +0900

    문자열 변환 방법 변경

--- admin/admin_function.js
+++ admin/admin_function.js
@@ -39,7 +39,7 @@ module.exports.cmd = async function (_cmd, returnRslt = false) {
             return (await exec(_cmd)).stdout.replace(/\u001b\[\d\dm/g, "").trimEnd();
         }
         catch (e) {
-            return e.toString();
+            return String(e);
         }
     }
     else {
