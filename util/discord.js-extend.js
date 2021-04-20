986e0bce0ab0c47e779b59e63c13faec544f09a7
cjh980402 <9804cjh@naver.com>
2021-04-20 09:41:37 +0900

    toLocaleUnitString 관련 수정

--- util/discord.js-extend.js
+++ util/discord.js-extend.js
@@ -13,16 +13,30 @@ Object.defineProperty(Discord.Message.prototype, 'fullContent', {
     }
 });
 
-Object.defineProperty(Number.prototype, 'toUnitString', {
-    value: function (count = 5, locale) {
-        // count는 출력할 단위의 개수
-        const localeUnitNames = { 'ko-KR': ['경', '조', '억', '만', ''], 'en-US': ['Trillion', 'Billion', 'Million', 'Thousand', ''] };
-        const localeUnitStd = { 'ko-KR': [10000000000000000, 10000], 'en-US': [1000000000000, 1000] };
+Object.defineProperty(Number.prototype, 'toLocaleUnitString', {
+    value: function (locales = Intl.NumberFormat().resolvedOptions().locale, count = 5) {
+        // locales는 출력할 형식의 로케일, count는 출력할 단위의 개수
+        const localeUnitNames = {
+            'ko-KR': ['경', '조', '억', '만', ''],
+            'ja-JP': ['京', '兆', '億', '万', ''],
+            'zh-CN': ['京', '兆', '亿', '万', ''],
+            'en-US': ['Trillion', 'Billion', 'Million', 'Thousand', '']
+        };
+        const localeUnitStd = {
+            'ko-KR': [10000000000000000, 10000],
+            'ja-JP': [10000000000000000, 10000],
+            'zh-CN': [10000000000000000, 10000],
+            'en-US': [1000000000000, 1000]
+        };
         const rslt = [];
         let num = +this; // 현재 Number 값의 복사본
 
-        const unitName = localeUnitNames[locale] ?? localeUnitNames['ko-KR'];
-        const unitStd = localeUnitStd[locale] ?? localeUnitStd['ko-KR'];
+        const unitName = localeUnitNames[locales];
+        const unitStd = localeUnitStd[locales];
+        if (!unitName || !unitStd) {
+            throw new RangeError('Incorrect locale information provided');
+        }
+
         for (let i = 0, unitNum = unitStd[0]; i < unitName.length; num %= unitNum, unitNum /= unitStd[1], i++) {
             const tmp = Math.floor(num / unitNum);
             if (tmp > 0 && rslt.length < count) {
