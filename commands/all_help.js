fa1754ac59ed4e95380c10f51a844c059f528d68
cjh980402 <9804cjh@naver.com>
2021-05-17 21:58:14 +0900

    도움말 기능을 특정 명령어에 대해서도 가능하게 개선

--- commands/all_help.js
+++ commands/all_help.js
@@ -11,17 +11,23 @@ function generateHelpEmbed(help) {
 }
 
 module.exports = {
-    usage: `${client.prefix}help (카테고리)`,
+    usage: `${client.prefix}help (카테고리│명령어 이름)`,
     command: ['help', 'h', '도움말', '명령어', 'ㄷㅇㅁ', 'ㅁㄹㅇ'],
-    description: '- 카테고리는 메이플, 음악, 기타가 있으며 생략시 모든 명령어의 도움말을 출력합니다.',
-    type: ['음악', '메이플', '기타'],
+    description: `- 카테고리나 명령어 이름을 입력하면 해당하는 명령어의 도움말을 출력합니다.
+카테고리는 메이플, 음악, 기타가 있으며 카테고리나 명령어 이름을 생략시 모든 명령어의 도움말을 출력합니다.`,
+    type: ['메이플', '음악', '기타'],
     async execute(message, args) {
+        // description이 없는 명령어는 히든 명령어
         if (args[0] && !this.type.includes(args[0])) {
-            return message.channel.send('지원하지 않는 도움말입니다.');
+            const target = client.commands.find((cmd) => cmd.command.includes(args[0]));
+            if (!target?.description) {
+                return message.channel.send('지원하지 않는 도움말입니다.');
+            } else {
+                return message.channel.send(`**${target.usage}**\n- 대체 명령어: ${target.command.join(', ')}\n${target.description}`);
+            }
         }
 
         const description = client.commands.filter((cmd) => cmd.description && (cmd.type.includes(args[0]) || !args[0])).map((cmd) => `**${cmd.usage}**\n- 대체 명령어: ${cmd.command.join(', ')}\n${cmd.description}`);
-        // description이 없는 명령어는 히든 명령어
 
         let currentPage = 0;
         const embeds = generateHelpEmbed(description);
