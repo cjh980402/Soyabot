ab036d199ac483cc0bc7c6e0d66217894ca65e28
cjh980402 <9804cjh@naver.com>
2021-07-11 21:11:36 +0900

    음악기능 리액션 관련 로직 변경

--- util/music_play.js
+++ util/music_play.js
@@ -2,7 +2,6 @@ const { createAudioPlayer, createAudioResource, StreamType } = require('@discord
 const { songDownload } = require('./song_util');
 const { replyAdmin } = require('../admin/bot_control');
 const { STAY_TIME, DEFAULT_VOLUME } = require('../soyabot_config.json');
-const { canModifyQueue } = require('./soyabot_util');
 const disconnectTimeout = {};
 
 module.exports.QueueElement = class {
@@ -14,6 +13,7 @@ module.exports.QueueElement = class {
     loop = false;
     playing = true;
     audioPlayer = createAudioPlayer();
+    playingMessage = null;
 
     constructor(textChannel, voiceChannel, connection, songs) {
         this.textChannel = textChannel;
@@ -53,6 +53,16 @@ module.exports.QueueElement = class {
             } catch {}
         }
     }
+
+    async deleteMessage() {
+        const find = await db.get('SELECT * FROM pruningskip WHERE channelid = ?', [this.voiceChannel.guild.id]);
+        if (!find) {
+            try {
+                await this.playingMessage?.delete();
+            } catch {}
+        }
+        this.playingMessage = null;
+    }
 };
 
 module.exports.play = async function (queue) {
@@ -86,24 +96,14 @@ module.exports.play = async function (queue) {
         return module.exports.play(queue);
     }
 
-    const playingMessage = await queue.textSend(`🎶 노래 재생 시작: **${song.title}**\n${song.url}`);
-    const filter = (_, user) => user.id !== client.user.id;
-    const collector = playingMessage?.createReactionCollector({ filter, time: song.duration > 0 ? song.duration * 1000 : 600000 }).once('end', async () => {
-        const find = await db.get('SELECT * FROM pruningskip WHERE channelid = ?', [guild.id]);
-        if (!find) {
-            try {
-                await playingMessage.delete();
-            } catch {}
-        }
-    });
-
+    queue.playingMessage = await queue.textSend(`🎶 노래 재생 시작: **${song.title}**\n${song.url}`);
     queue.audioPlayer.play(resource);
     queue.audioPlayer.state.resource.volume.setVolume(queue.volume / 100);
     queue.audioPlayer
         .on('stateChange', (oldState, newState) => {
             if (newState.status === 'idle' && oldState.status !== 'idle') {
                 // 재생 중인 노래가 끝난 경우
-                collector?.stop();
+                queue.deleteMessage();
                 queue.audioPlayer.removeAllListeners('stateChange');
                 queue.audioPlayer.removeAllListeners('error');
                 if (queue.loop) {
@@ -115,7 +115,7 @@ module.exports.play = async function (queue) {
             }
         })
         .on('error', async (e) => {
-            collector?.stop();
+            queue.deleteMessage();
             queue.audioPlayer.removeAllListeners('stateChange');
             queue.audioPlayer.removeAllListeners('error');
             queue.textSend('재생할 수 없는 동영상입니다.');
@@ -125,74 +125,12 @@ module.exports.play = async function (queue) {
         });
 
     try {
-        await playingMessage.react('⏯');
-        await playingMessage.react('⏭');
-        await playingMessage.react('🔇');
-        await playingMessage.react('🔉');
-        await playingMessage.react('🔊');
-        await playingMessage.react('🔁');
-        await playingMessage.react('⏹');
-    } catch {
-        return; // 에러 발생 시 종료
-    }
-
-    collector.on('collect', async (reaction, user) => {
-        try {
-            await reaction.users.remove(user);
-            if (queue.audioPlayer.state.status === 'idle' || queue.connection.state.status !== 'ready') {
-                return collector.stop();
-            }
-            if (!canModifyQueue(await guild.members.fetch(user.id, false))) {
-                return queue.textSend(`${client.user}과 같은 음성 채널에 참가해주세요!`);
-            }
-
-            switch (reaction.emoji.name) {
-                case '⏯':
-                    queue.playing = !queue.playing;
-                    if (queue.playing) {
-                        queue.audioPlayer.unpause();
-                        queue.textSend(`${user} ▶️ 노래를 다시 틀었습니다.`);
-                    } else {
-                        queue.audioPlayer.pause();
-                        queue.textSend(`${user} ⏸ 노래를 일시정지 했습니다.`);
-                    }
-                    break;
-                case '⏭':
-                    queue.textSend(`${user} ⏭ 노래를 건너뛰었습니다.`);
-                    queue.playing = true;
-                    queue.audioPlayer.stop(true);
-                    break;
-                case '🔇':
-                    queue.volume = queue.volume <= 0 ? DEFAULT_VOLUME : 0;
-                    queue.audioPlayer.state.resource.volume.setVolume(queue.volume / 100);
-                    queue.textSend(queue.volume ? `${user} 🔊 음소거를 해제했습니다.` : `${user} 🔇 노래를 음소거 했습니다.`);
-                    break;
-                case '🔉':
-                    queue.volume = Math.max(queue.volume - 10, 0);
-                    queue.audioPlayer.state.resource.volume.setVolume(queue.volume / 100);
-                    queue.textSend(`${user} 🔉 음량을 낮췄습니다. 현재 음량: ${queue.volume}%`);
-                    break;
-                case '🔊':
-                    queue.volume = Math.min(queue.volume + 10, 100);
-                    queue.audioPlayer.state.resource.volume.setVolume(queue.volume / 100);
-                    queue.textSend(`${user} 🔊 음량을 높였습니다. 현재 음량: ${queue.volume}%`);
-                    break;
-                case '🔁':
-                    queue.loop = !queue.loop;
-                    queue.textSend(`현재 반복 재생 상태: ${queue.loop ? '**ON**' : '**OFF**'}`);
-                    break;
-                case '⏹':
-                    queue.textSend(`${user} ⏹ 노래를 정지했습니다.`);
-                    queue.songs = [];
-                    try {
-                        queue.audioPlayer.stop(true);
-                    } catch {
-                        queue.connection.destroy();
-                    }
-                    break;
-            }
-        } catch {
-            return queue.textSend('**권한이 없습니다 - [ADD_REACTIONS, MANAGE_MESSAGES]**');
-        }
-    });
+        await queue.playingMessage.react('⏯');
+        await queue.playingMessage.react('⏭');
+        await queue.playingMessage.react('🔇');
+        await queue.playingMessage.react('🔉');
+        await queue.playingMessage.react('🔊');
+        await queue.playingMessage.react('🔁');
+        await queue.playingMessage.react('⏹');
+    } catch {}
 };
