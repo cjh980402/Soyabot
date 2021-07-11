6c33ab560c67a0714ad17796f36c51555e8b7e64
cjh980402 <9804cjh@naver.com>
2021-07-12 00:06:28 +0900

    음악 기능 관련 코드 이동

--- util/music_play.js
+++ util/music_play.js
@@ -1,6 +1,7 @@
 const { songDownload } = require('./song_util');
 const { replyAdmin } = require('../admin/bot_control');
 const { STAY_TIME, DEFAULT_VOLUME } = require('../soyabot_config.json');
+const { canModifyQueue } = require('./soyabot_util');
 const disconnectTimeout = {};
 
 module.exports.QueueElement = class {
@@ -137,4 +138,112 @@ module.exports.play = async function (queue) {
         await queue.playingMessage.react('🔁');
         await queue.playingMessage.react('⏹');
     } catch {}
+};
+
+module.exports.musicReactionControl = async function (reaction, user) {
+    const { guild } = reaction.message.channel;
+    const queue = client.queues.get(guild?.id);
+    try {
+        if (user.bot || queue?.playingMessage?.id !== reaction.message.id) {
+            return;
+        }
+
+        await reaction.users.remove(user);
+        if (!queue.connection.dispatcher) {
+            return queue.deleteMessage();
+        }
+        if (!canModifyQueue(await guild.members.fetch(user.id, false))) {
+            return queue.textSend(`${client.user}과 같은 음성 채널에 참가해주세요!`);
+        }
+
+        switch (reaction.emoji.name) {
+            case '⏯':
+                queue.playing = !queue.playing;
+                if (queue.playing) {
+                    queue.connection.dispatcher.resume();
+                    queue.textSend(`${user} ▶️ 노래를 다시 틀었습니다.`);
+                } else {
+                    queue.connection.dispatcher.pause(true);
+                    queue.textSend(`${user} ⏸ 노래를 일시정지 했습니다.`);
+                }
+                break;
+            case '⏭':
+                queue.textSend(`${user} ⏭ 노래를 건너뛰었습니다.`);
+                queue.playing = true;
+                queue.connection.dispatcher.end();
+                break;
+            case '🔇':
+                queue.volume = queue.volume <= 0 ? DEFAULT_VOLUME : 0;
+                queue.connection.dispatcher.setVolume(queue.volume / 100);
+                queue.textSend(queue.volume ? `${user} 🔊 음소거를 해제했습니다.` : `${user} 🔇 노래를 음소거 했습니다.`);
+                break;
+            case '🔉':
+                queue.volume = Math.max(queue.volume - 10, 0);
+                queue.connection.dispatcher.setVolume(queue.volume / 100);
+                queue.textSend(`${user} 🔉 음량을 낮췄습니다. 현재 음량: ${queue.volume}%`);
+                break;
+            case '🔊':
+                queue.volume = Math.min(queue.volume + 10, 100);
+                queue.connection.dispatcher.setVolume(queue.volume / 100);
+                queue.textSend(`${user} 🔊 음량을 높였습니다. 현재 음량: ${queue.volume}%`);
+                break;
+            case '🔁':
+                queue.loop = !queue.loop;
+                queue.textSend(`현재 반복 재생 상태: ${queue.loop ? '**ON**' : '**OFF**'}`);
+                break;
+            case '⏹':
+                queue.textSend(`${user} ⏹ 노래를 정지했습니다.`);
+                queue.songs = [];
+                try {
+                    queue.connection.dispatcher.end();
+                } catch {
+                    queue.connection.disconnect();
+                }
+                break;
+        }
+    } catch {
+        return queue.textSend('**권한이 없습니다 - [ADD_REACTIONS, MANAGE_MESSAGES]**');
+    }
+};
+
+module.exports.musicActiveControl = function (oldState, newState) {
+    try {
+        const oldVoice = oldState?.channel;
+        const newVoice = newState?.channel;
+        if (oldVoice !== newVoice) {
+            console.log(!oldVoice ? 'User joined!' : !newVoice ? 'User left!' : 'User switched channels!');
+
+            if (newVoice) {
+                const newQueue = client.queues.get(newVoice.guild.id);
+                if (newQueue?.connection.dispatcher && !newQueue.playing && newVoice.id === newQueue.voiceChannel.id && newVoice.members.size === 2 && newVoice.members.first().id === client.user.id) {
+                    newQueue.playing = true;
+                    newQueue.connection.dispatcher.resume();
+                    newQueue.textSend('대기열을 다시 재생합니다.');
+                }
+            }
+
+            if (oldVoice) {
+                const oldQueue = client.queues.get(oldVoice.guild.id);
+                if (oldQueue?.connection.dispatcher && oldVoice.id === oldQueue.voiceChannel.id && oldVoice.members.size === 1 && oldVoice.members.first().id === client.user.id) {
+                    // 봇만 음성 채널에 있는 경우
+                    if (oldQueue.playing) {
+                        oldQueue.playing = false;
+                        oldQueue.connection.dispatcher.pause(true);
+                        oldQueue.textSend('모든 사용자가 음성채널을 떠나서 대기열을 일시정지합니다.');
+                    }
+                    setTimeout(() => {
+                        const queue = client.queues.get(oldVoice.guild.id);
+                        if (queue?.connection.dispatcher && oldVoice.id === queue.voiceChannel.id && oldVoice.members.size === 1 && oldVoice.members.first().id === client.user.id) {
+                            // 5분이 지나도 봇만 음성 채널에 있는 경우
+                            queue.textSend(`5분 동안 ${client.user.username}이 비활성화 되어 대기열을 끝냅니다.`);
+                            queue.songs = [];
+                            queue.connection.dispatcher.end();
+                        }
+                    }, 300000);
+                }
+            }
+        }
+    } catch (e) {
+        replyAdmin(`[oldState]\n${oldState?._p}\n[newState]\n${newState?._p}\n에러 내용: ${e}\n${e.stack ?? e._p}`);
+    }
 };
