17c98dbd8a7f8c2bd1c1b9f1df542979d12932ec
cjh980402 <9804cjh@naver.com>
2021-07-24 21:31:39 +0900

    실시간 검색어 사이트 변경 대응

--- commands/others_realtime_keyword.js
+++ commands/others_realtime_keyword.js
@@ -6,7 +6,7 @@ module.exports = {
     description: '- https://www.signal.bz 기준 네이버 실시간 검색어를 보여줍니다.',
     type: ['기타'],
     async execute(message) {
-        const data = await (await fetch('https://api.signal.bz/news/realtimen')).json();
-        return message.channel.send(`실시간 검색어\n${new Date().toLocaleString()}\n\n${data.map((v) => `${v.rank}. ${v.keyword}`).join('\n')}`);
+        const data = await (await fetch('https://api.signal.bz/news/realtime')).json();
+        return message.channel.send(`실시간 검색어\n${new Date().toLocaleString()}\n\n${data.top10.map((v) => `${v.rank}. ${v.keyword}`).join('\n')}`);
     }
 };
