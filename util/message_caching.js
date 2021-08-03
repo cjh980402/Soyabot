01c9d8f903f17975907d72ec81b1ce8e631cd483
cjh980402 <9804cjh@naver.com>
2021-08-03 09:27:25 +0900

    슬래시 명령어 캐싱 추가

--- util/message_caching.js
+++ util/message_caching.js
@@ -1,15 +1,30 @@
-const { Message } = require('./discord.js-extend');
+const { CommandInteraction, Message } = require('./discord.js-extend');
 
-module.exports = async function (message) {
-    if (!(message instanceof Message && message.guild)) {
-        return;
+module.exports = async function (messageOrInteraction) {
+    if (messageOrInteraction instanceof Message) {
+        if (!messageOrInteraction.guild) {
+            return;
+        }
+        const senderKey = `${messageOrInteraction.guild.id} ${messageOrInteraction.author.id}`;
+        const messagestat = await db.get('SELECT * FROM messagedb WHERE channelsenderid = ?', [senderKey]);
+        await db.replace('messagedb', {
+            channelsenderid: senderKey,
+            messagecnt: (messagestat?.messagecnt ?? 0) + 1,
+            lettercnt: (messagestat?.lettercnt ?? 0) + messageOrInteraction.content.replace(/[\s\u2007\u200b\u202d\u3164]/g, '').length,
+            lastmessage: messageOrInteraction.content
+        });
+    } else if (messageOrInteraction instanceof CommandInteraction) {
+        if (!messageOrInteraction.guild) {
+            return;
+        }
+        const senderKey = `${messageOrInteraction.guild.id} ${messageOrInteraction.user.id}`;
+        const messagestat = await db.get('SELECT * FROM messagedb WHERE channelsenderid = ?', [senderKey]);
+        const content = `/${messageOrInteraction.commandName}${messageOrInteraction.options._hoistedOptions.map((v) => ` ${v.value}`).join('')}`;
+        await db.replace('messagedb', {
+            channelsenderid: senderKey,
+            messagecnt: (messagestat?.messagecnt ?? 0) + 1,
+            lettercnt: (messagestat?.lettercnt ?? 0) + content.replace(/[\s\u2007\u200b\u202d\u3164]/g, '').length,
+            lastmessage: content
+        });
     }
-    const senderKey = `${message.guild.id} ${message.author.id}`;
-    const messagestat = await db.get('SELECT * FROM messagedb WHERE channelsenderid = ?', [senderKey]);
-    await db.replace('messagedb', {
-        channelsenderid: senderKey,
-        messagecnt: (messagestat?.messagecnt ?? 0) + 1,
-        lettercnt: (messagestat?.lettercnt ?? 0) + message.content.replace(/[\s\u2007\u200b\u202d\u3164]/g, '').length,
-        lastmessage: message.content
-    });
 };
