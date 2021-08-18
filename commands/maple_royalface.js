dfd18f1cb08bab7a858f8b920441a17074324a21
cjh980402 <9804cjh@naver.com>
2021-08-18 22:01:54 +0900

    헤어, 성형 기능의 슬래시 명령어 구조 변경

--- commands/maple_royalface.js
+++ commands/maple_royalface.js
@@ -38,46 +38,65 @@ module.exports = {
     },
     commandData: {
         name: '성형',
-        description: `해당 성별의 목표 성형을 얻을 때까지 로얄 성형 시뮬을 수행합니다.(참고. ${client.prefix}성형 확률)`,
+        description: '로얄 성형 관련 기능을 수행합니다.',
         options: [
             {
-                name: '성별',
-                type: 'STRING',
-                description: '성형 시뮬레이션을 수행할 성별',
-                required: true,
-                choices: ['남', '여', '확률'].map((v) => ({ name: v, value: v }))
+                name: '확률',
+                type: 'SUB_COMMAND',
+                description: '현재 로얄 성형의 확률을 보여줍니다.'
             },
             {
-                name: '목표_성형_이름',
-                type: 'STRING',
-                description: '시뮬레이션의 목표 성형',
-                choices: [...faceList['남'], ...faceList['여']].deduplication().map((v) => ({ name: v, value: v }))
+                name: '남',
+                type: 'SUB_COMMAND',
+                description: '남자 캐릭터의 성형 시뮬레이션을 수행합니다.',
+                options: [
+                    {
+                        name: '목표_성형_이름',
+                        type: 'STRING',
+                        description: '시뮬레이션의 목표 성형',
+                        required: true,
+                        choices: faceList['남'].map((v) => ({ name: v, value: v }))
+                    }
+                ]
+            },
+            {
+                name: '여',
+                type: 'SUB_COMMAND',
+                description: '여자 캐릭터의 성형 시뮬레이션을 수행합니다.',
+                options: [
+                    {
+                        name: '목표_성형_이름',
+                        type: 'STRING',
+                        description: '시뮬레이션의 목표 성형',
+                        required: true,
+                        choices: faceList['여'].map((v) => ({ name: v, value: v }))
+                    }
+                ]
             }
         ]
     },
     async commandExecute(interaction) {
-        const args = interaction.options.data.map((v) => v.value);
+        const subcommand = interaction.options.getSubcommand();
 
-        if (args[0] === '확률' || args[0] === 'ㅎㄹ') {
+        if (subcommand === '확률') {
             const rslt = `<로얄 성형 확률>\n\n- 남자 성형\n${faceList['남'].map((v) => `${v}: 16.67%`).join('\n')}\n\n- 여자 성형\n${faceList['여'].map((v) => `${v}: 16.67%`).join('\n')}`;
             return interaction.followUp(rslt);
-        }
-        const gender = args.shift();
-        const goalface = (faceList[gender] ?? []).findIndex((v) => v === args[0]);
-        if (goalface === -1) {
-            return interaction.followUp(`**${this.usage}**\n- 대체 명령어: ${this.command.join(', ')}\n${this.description}`);
-        }
-        // gender은 성별, goalface는 목표 성형의 인덱스
-        // random은 0이상 1미만
-        const list = []; // 진행 과정 담을 배열 (인덱스 저장)
+        } else {
+            const goalface = faceList[subcommand].indexOf(interaction.options.getString('목표_성형_이름'));
+            // subcommand은 성별, goalface는 목표 성형의 인덱스
+            // random은 0이상 1미만
+            const list = []; // 진행 과정 담을 배열 (인덱스 저장)
 
-        while (list[list.length - 1] !== goalface) {
-            // 목표 성형을 띄웠으면 종료
-            const now = Math.floor(Math.random() * (faceList[gender].length - +(list.length > 0)));
-            list.push(now + +(list[list.length - 1] <= now)); // 현재 뜬 성형의 인덱스 저장, now 뒤에 더하는 이유는 최근 성형 제외 목적
-        }
+            while (list[list.length - 1] !== goalface) {
+                // 목표 성형을 띄웠으면 종료
+                const now = Math.floor(Math.random() * (faceList[subcommand].length - +(list.length > 0)));
+                list.push(now + +(list[list.length - 1] <= now)); // 현재 뜬 성형의 인덱스 저장, now 뒤에 더하는 이유는 최근 성형 제외 목적
+            }
 
-        const rslt = `로얄 성형 (목표: ${faceList[gender][goalface]}) 결과\n\n수행 횟수: ${list.length}회\n\n진행 과정\n${list.map((v, i) => `${i + 1}번째: ${faceList[gender][v]}`).join('\n')}`;
-        return interaction.followUp(rslt);
+            const rslt = `로얄 성형 (목표: ${faceList[subcommand][goalface]}) 결과\n\n수행 횟수: ${list.length}회\n\n진행 과정\n${list
+                .map((v, i) => `${i + 1}번째: ${faceList[subcommand][v]}`)
+                .join('\n')}`;
+            return interaction.followUp(rslt);
+        }
     }
 };
