e27ad200dd403968b743b23ab01775aef108297c
cjh980402 <9804cjh@naver.com>
2021-08-23 23:42:02 +0900

    그림 기능 처리 개선

--- commands/others_portrait.js
+++ commands/others_portrait.js
@@ -1,5 +1,5 @@
 const { MessageAttachment } = require('../util/discord.js-extend');
-const { /*ADMIN_ID, */ BOT_SERVER_DOMAIN } = require('../soyabot_config.json');
+const { BOT_SERVER_DOMAIN } = require('../soyabot_config.json');
 const fetch = require('node-fetch');
 const { getMessageImage } = require('../util/soyabot_util');
 // const { cmd } = require('../admin/admin_function');
@@ -7,7 +7,7 @@ const { getMessageImage } = require('../util/soyabot_util');
 module.exports = {
     usage: `${client.prefix}그림`,
     command: ['그림', 'ㄱㄹ'],
-    // description: '- 사진을 흑백 스케치화 해줍니다.',
+    description: '- 사진을 흑백 스케치화 해줍니다.',
     type: ['기타'],
     async messageExecute(message) {
         const imageURL = await getMessageImage(message);
@@ -17,8 +17,13 @@ module.exports = {
             /*const { stdout: portraitPic } = await cmd(`python3 ./util/gl2face_portrait.py ${imageURL}`, { encoding: 'buffer' }); // 파이썬 스크립트 실행
             const image = new MessageAttachment(portraitPic, 'portrait.png');*/
 
-            const image = new MessageAttachment(await (await fetch(`http://${BOT_SERVER_DOMAIN}/portrait/${encodeURIComponent(imageURL)}`)).buffer(), 'portrait.png');
-            return message.channel.send({ files: [image] });
+            const response = await fetch(`http://${BOT_SERVER_DOMAIN}/portrait/${encodeURIComponent(imageURL)}`);
+            if (response.status === 200) {
+                const image = new MessageAttachment(await response.buffer(), 'portrait.png');
+                return message.channel.send({ files: [image] });
+            } else {
+                return message.channel.send('그림 작업을 실패하였습니다.');
+            }
         }
     }
 };
