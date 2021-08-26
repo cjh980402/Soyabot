7bff000fa39f487975edf24d278593b2c0065a79
cjh980402 <9804cjh@naver.com>
2021-08-26 10:06:38 +0900

    VoiceConnection 처리 개선

--- util/music_play.js
+++ util/music_play.js
@@ -89,6 +89,9 @@ module.exports.QueueElement = class {
             this.playingMessage = await this.sendMessage({ embeds: [embed], components: [row1, row2] });
             this.subscription.player.play(await songDownload(this.songs[0].url));
             this.subscription.player.state.resource.volume.setVolume(this.volume / 100);
+            if (this.subscription.connection.state.status !== VoiceConnectionStatus.Ready) {
+                this.subscription.player.stop(true); // 노래 재생 후 VoiceConnection이 끊어진 경우 재생 종료
+            }
         } catch (err) {
             this.sendMessage('노래 재생을 실패했습니다.');
             replyAdmin(`노래 재생 에러\nsong 객체: ${this.songs[0]._p}\n에러 내용: ${err.stack ?? err._p}`);
