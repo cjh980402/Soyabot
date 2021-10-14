2c32e7934acebc45f8780c654ef42c4b7d4930ca
cjh980402 <9804cjh@naver.com>
2021-10-14 09:48:10 +0900

    실검 API 주소 변경 대응

--- commands/others_realtime_keyword.js
+++ commands/others_realtime_keyword.js
@@ -5,7 +5,7 @@ export const command = ['실검', 'ㅅㄱ'];
 export const description = '- https://www.signal.bz 기준 네이버 실시간 검색어를 보여줍니다.';
 export const type = ['기타'];
 export async function messageExecute(message) {
-    const data = await (await fetch('https://api.signal.bz/news/realtime')).json();
+    const data = await (await fetch('https://test-api.signal.bz/news/realtime')).json();
     return message.channel.send(`실시간 검색어\n${new Date().toLocaleString()}\n\n${data.top10.map((v) => `${v.rank}. ${v.keyword}`).join('\n')}`);
 }
 export const commandData = {
@@ -13,6 +13,6 @@ export const commandData = {
     description: 'https://www.signal.bz 기준 네이버 실시간 검색어를 보여줍니다.'
 };
 export async function commandExecute(interaction) {
-    const data = await (await fetch('https://api.signal.bz/news/realtime')).json();
+    const data = await (await fetch('https://test-api.signal.bz/news/realtime')).json();
     return interaction.followUp(`실시간 검색어\n${new Date().toLocaleString()}\n\n${data.top10.map((v) => `${v.rank}. ${v.keyword}`).join('\n')}`);
 }
