5eaf3a24cab29a3e2b4257a9888b9d416fb3e126
cjh980402 <9804cjh@naver.com>
2021-12-13 15:37:33 +0900

    홈페이지 개편에 따른 학식 기능 대응

--- commands/others_haksik.js
+++ commands/others_haksik.js
@@ -1,5 +1,26 @@
 import fetch from 'node-fetch';
 import { load } from 'cheerio';
+import { MessageActionRow, MessageButton, MessageEmbed } from '../util/discord.js-extend.js';
+
+function getHaksikEmbed(date, haksik) {
+    const embeds = [];
+    for (let i = 0; i < date.length; i++) {
+        const embed = new MessageEmbed()
+            .setTitle(`**${date[i]}**`)
+            .setColor('#FF9999')
+            .setDescription(
+                haksik
+                    .find('td.al')
+                    .get(i + 1)
+                    .children.map((v) => v.data ?? '\n')
+                    .join('')
+                    .trim()
+            )
+            .setTimestamp();
+        embeds.push(embed);
+    }
+    return embeds;
+}
 
 export const usage = `${client.prefix}학식 (요일)`;
 export const command = ['학식', 'ㅎㅅ'];
@@ -15,27 +36,52 @@ export async function messageExecute(message, args) {
     }
 
     const data = load(await (await fetch('https://www.uos.ac.kr/food/placeList.do')).text())('#week tr');
-    if (data.length >= 3) {
+    if (data.length > 0) {
         // 하루 이상의 학식 데이터가 존재
-        for (let i = 0; i < data.length; i += 3) {
-            const date = data.eq(i).find('th[rowspan="3"]').text();
+        for (let i = 0; i < data.length; i++) {
+            const nowData = data.eq(i);
+            const date = nowData.find('th[scope="row"]').eq(0).text();
             if (date.includes(day)) {
-                await message.channel.send(
-                    `${date}의 점심!\n${data
-                        .eq(i + 1)
-                        .find('td.al')
-                        .html()
-                        .decodeHTML()
-                        .trim()}`
+                const embeds = getHaksikEmbed([`${date}의 점심`, `${date}의 저녁`], nowData);
+
+                let currentPage = 0;
+                const row = new MessageActionRow().addComponents(
+                    new MessageButton().setCustomId('prev').setEmoji('⬅️').setStyle('SECONDARY'),
+                    new MessageButton().setCustomId('stop').setEmoji('⏹️').setStyle('SECONDARY'),
+                    new MessageButton().setCustomId('next').setEmoji('➡️').setStyle('SECONDARY')
                 );
-                return message.channel.send(
-                    `${date}의 저녁!\n${data
-                        .eq(i + 2)
-                        .find('td.al')
-                        .html()
-                        .decodeHTML()
-                        .trim()}`
-                );
+                const haksikEmbed = await message.channel.send({
+                    content: `**현재 페이지 - ${currentPage + 1}/${embeds.length}**`,
+                    embeds: [embeds[currentPage]],
+                    components: [row]
+                });
+
+                const filter = (itr) => message.author.id === itr.user.id;
+                const collector = haksikEmbed.createMessageComponentCollector({ filter, time: 60000 });
+
+                return collector.on('collect', async (itr) => {
+                    try {
+                        switch (itr.customId) {
+                            case 'next':
+                                currentPage = (currentPage + 1) % embeds.length;
+                                await haksikEmbed.edit({
+                                    content: `**현재 페이지 - ${currentPage + 1}/${embeds.length}**`,
+                                    embeds: [embeds[currentPage]]
+                                });
+                                break;
+                            case 'prev':
+                                currentPage = (currentPage - 1 + embeds.length) % embeds.length;
+                                await haksikEmbed.edit({
+                                    content: `**현재 페이지 - ${currentPage + 1}/${embeds.length}**`,
+                                    embeds: [embeds[currentPage]]
+                                });
+                                break;
+                            case 'stop':
+                                collector.stop();
+                                break;
+                        }
+                    } catch {}
+                });
             }
         }
         return message.channel.send(`${day}요일은 학식이 제공되지 않습니다.`);
