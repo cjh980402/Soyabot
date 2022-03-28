4841d8b3c6368b2ee3beb94fae909a58e6185a39
cjh980402 <9804cjh@naver.com>
2022-03-28 13:40:14 +0900

    샤드 실행 에러 처리 추가

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { TOKEN } from './soyabot_config.js';
 
 const manager = new ShardingManager('./bot.js', { token: TOKEN });
 
-manager.on('shardCreate', (shard) => console.log(`Launched shard ${shard.id}`));
+manager.on('shardCreate', (shard) => console.log(`${shard.id}번째 샤드 생성 완료`));
 
-manager.spawn();
+try {
+    await manager.spawn();
+} catch (err) {
+    console.error('샤드 실행 에러 발생:', err);
+}
