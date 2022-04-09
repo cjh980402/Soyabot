24bbc54c9ea4373ed289df1ebc098a310a1dbcd7
cjh980402 <9804cjh@naver.com>
2022-04-09 23:25:39 +0900

    불필요한 함수 호출 제거

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 import { SoyaClient } from './classes/SoyaClient.js';
 import { readdirSync } from 'node:fs';
 import { TOKEN } from './soyabot_config.js';
-import { initClient, exec } from './admin/admin_function.js';
+import { initClient } from './admin/admin_function.js';
 const client = new SoyaClient('./db/soyabot_data.db');
 
 // node.js v15부터 Unhandled promise rejection이 발생하면 프로세스를 비정상 종료시키므로 처리를 해야함
@@ -27,5 +27,4 @@ try {
     // await client.application.commands.set(datas); // 인터랙션 데이터 변경 시에만 활성화하기
 } catch (err) {
     console.error('로그인 에러 발생:', err);
-    await exec('npm stop');
 }
--- events/error.js
+++ events/error.js
@@ -1,9 +1,4 @@
-import { setTimeout } from 'node:timers/promises';
-import { exec } from '../admin/admin_function.js';
-
 export const name = 'error';
-export async function listener() {
+export function listener(err) {
     console.error('클라이언트 에러 발생:', err);
-    await setTimeout(30000); // 30초 대기
-    await exec('npm restart'); // 재시작
 }
