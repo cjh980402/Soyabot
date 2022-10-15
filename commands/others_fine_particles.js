d0c20d78219344dc09ba8b865945014a73586f9f
cjh980402 <9804cjh@naver.com>
2022-10-16 04:02:08 +0900

    미세먼지 기능 사이트 변경 대응 및 개선

--- commands/others_fine_particles.js
+++ commands/others_fine_particles.js
@@ -1,28 +1,5 @@
-import { EmbedBuilder } from 'discord.js';
 import { request } from 'undici';
 import { load } from 'cheerio';
-import { sendPageMessage } from '../util/soyabot_util.js';
-
-async function getFineParticlesEmbed() {
-    const dustTypes = ['미세먼지', '초미세먼지'];
-    const embeds = [];
-    for (const dustType of dustTypes) {
-        const { body } = await request(
-            `https://m.search.daum.net/search?w=tot&nil_mtopsearch=btn&DA=YZR&q=${encodeURIComponent(
-                dustType
-            )}%EC%98%81%EC%83%81`
-        );
-        const $ = load(await body.text());
-
-        const embed = new EmbedBuilder()
-            .setTitle(`**현재 ${dustType} 지도**`)
-            .setColor('#FF9999')
-            .setImage($('div.play_video > img').attr('data-original-src'))
-            .setTimestamp();
-        embeds.push(embed);
-    }
-    return embeds;
-}
 
 export const type = '기타';
 export const commandData = {
@@ -30,6 +7,11 @@ export const commandData = {
     description: '현재 한국의 미세먼지 현황을 보여줍니다.'
 };
 export async function commandExecute(interaction) {
-    const embeds = await getFineParticlesEmbed();
-    await sendPageMessage(interaction, embeds);
+    const { body } = await request('https://www.airkorea.or.kr/web/dustForecast?pMENU_NO=113');
+    const $ = load(await body.text());
+
+    await interaction.followUp({
+        content: '대기질 농도 전망',
+        files: [`https://www.airkorea.or.kr${$('.model.MgT20 > .st_2 .popup-layer').attr('href')}`]
+    });
 }
