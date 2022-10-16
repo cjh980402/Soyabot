1013fe4d2c9989c715778b492d339304bb603075
cjh980402 <9804cjh@naver.com>
2022-10-16 23:10:49 +0900

    변수 이름 변경 및 불필요한 embed 필드 제거

--- commands/others_corona_statistic.js
+++ commands/others_corona_statistic.js
@@ -8,12 +8,12 @@ export const commandData = {
     description: '최신 기준 코로나 국내 현황 통계를 알려줍니다.'
 };
 export async function commandExecute(interaction) {
-    const { body: countBody } = await request('https://ncov.kdca.go.kr');
-    const countData = load(await countBody.text());
-    const today = countData('.occurrenceStatus .occur_graph tbody span');
-    const accumulated = countData('.occurrenceStatus .occur_num .box');
+    const { body } = await request('https://ncov.kdca.go.kr');
+    const $ = load(await body.text());
+    const today = $('.occurrenceStatus .occur_graph tbody span');
+    const accumulated = $('.occurrenceStatus .occur_num .box');
 
-    const updateDate = /\((.+ 기준).+\)/.exec(countData('.occurrenceStatus .livedate').text())[1];
+    const updateDate = /\((.+ 기준).+\)/.exec($('.occurrenceStatus .livedate').text())[1];
     const coronaEmbed = new EmbedBuilder()
         .setTitle(`**${updateDate}**`)
         .setThumbnail('attachment://mohw.png')
@@ -24,8 +24,7 @@ export async function commandExecute(interaction) {
             { name: '**사망자**', value: `${accumulated.eq(0).contents().eq(1).text()} (⬆️ ${today.eq(1).text()})` },
             { name: '**신규 입원**', value: `${today.eq(3).text()}` },
             { name: '**재원 위중증**', value: `${today.eq(2).text()}` }
-        ])
-        .setTimestamp();
+        ]);
 
     const thumbnail = new AttachmentBuilder('./pictures/mohw.png');
     await interaction.followUp({ embeds: [coronaEmbed], files: [thumbnail] });
