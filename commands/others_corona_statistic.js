789fd802ab37b0c821759b6f433e2d41513b30b7
cjh980402 <9804cjh@naver.com>
2022-10-16 20:15:20 +0900

    코로나 기능 오류 수정

--- commands/others_corona_statistic.js
+++ commands/others_corona_statistic.js
@@ -1,19 +1,24 @@
 import { AttachmentBuilder, EmbedBuilder } from 'discord.js';
 import { request } from 'undici';
 import { load } from 'cheerio';
-import { sendPageMessage } from '../util/soyabot_util.js';
 
-async function getCoronaEmbed() {
-    const { body: countBody } = await request('http://ncov.mohw.go.kr');
+export const type = '기타';
+export const commandData = {
+    name: '코로나',
+    description: '최신 기준 코로나 국내 현황 통계를 알려줍니다.'
+};
+export async function commandExecute(interaction) {
+    const { body: countBody } = await request('https://ncov.kdca.go.kr');
     const countData = load(await countBody.text());
     const today = countData('.occurrenceStatus .occur_graph tbody span');
     const accumulated = countData('.occurrenceStatus .occur_num .box');
+
     const updateDate = /\((.+ 기준).+\)/.exec(countData('.occurrenceStatus .livedate').text())[1];
-    const corona1 = new EmbedBuilder()
+    const coronaEmbed = new EmbedBuilder()
         .setTitle(`**${updateDate}**`)
         .setThumbnail('attachment://mohw.png')
         .setColor('#FF9999')
-        .setURL('http://ncov.mohw.go.kr')
+        .setURL('https://ncov.kdca.go.kr')
         .addFields([
             { name: '**확진 환자**', value: `${accumulated.eq(1).contents().eq(1).text()} (⬆️ ${today.eq(4).text()})` },
             { name: '**사망자**', value: `${accumulated.eq(0).contents().eq(1).text()} (⬆️ ${today.eq(1).text()})` },
@@ -22,36 +27,6 @@ async function getCoronaEmbed() {
         ])
         .setTimestamp();
 
-    const { body: countryBody } = await request('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13');
-    const countryData = load(await countryBody.text());
-    const rslt = countryData('.data_table.midd.mgt24 tbody tr')
-        .map((_, v) => ({
-            name: countryData(v).find('th').text(),
-            todayCountry: countryData(v).find('td[headers="status_level l_type2"]').text(),
-            todayAbroad: countryData(v).find('td[headers="status_level l_type3"]').text(),
-            accumulatedCase: countryData(v).find('td[headers="status_con s_type1"]').text()
-        }))
-        .get()
-        .sort((a, b) => +b.todayCountry.replace(/,/g, '') - +a.todayCountry.replace(/,/g, ''))
-        .map((v) => `${v.name}: ${v.accumulatedCase} (국내: ⬆️ ${v.todayCountry}, 해외: ⬆️ ${v.todayAbroad})`);
-    const corona2 = new EmbedBuilder()
-        .setTitle('**지역별 확진 환자 현황**')
-        .setThumbnail('attachment://mohw.png')
-        .setColor('#FF9999')
-        .setURL('http://ncov.mohw.go.kr')
-        .setDescription(`${rslt.shift()}\n\n${rslt.join('\n')}`)
-        .setTimestamp();
-
-    return [corona1, corona2];
-}
-
-export const type = '기타';
-export const commandData = {
-    name: '코로나',
-    description: '최신 기준 코로나 국내 현황 통계를 알려줍니다.'
-};
-export async function commandExecute(interaction) {
     const thumbnail = new AttachmentBuilder('./pictures/mohw.png');
-    const embeds = await getCoronaEmbed();
-    await sendPageMessage(interaction, embeds, { files: [thumbnail] });
+    await interaction.followUp({ embeds: [coronaEmbed], files: [thumbnail] });
 }
