e970c6bc96486d9afa0fb5bc5909aecc83d6de4a
cjh980402 <9804cjh@naver.com>
2023-06-24 10:27:06 +0900

    도원경 심볼 비용 추가

--- commands/maple_calc_symbol.js
+++ commands/maple_calc_symbol.js
@@ -1,43 +1,61 @@
 import { ApplicationCommandOptionType } from 'discord.js';
 
-const costConstant = [
+const symbolConstant = [
     {
         default: 80000,
-        diff: 1000
+        diff: 1000,
+        name: '소멸의 여로'
     },
     {
         default: 100000,
-        diff: 1000
+        diff: 1000,
+        name: '츄츄 아일랜드'
     },
     {
         default: 120000,
-        diff: 1000
+        diff: 1000,
+        name: '레헬른'
     },
     {
         default: 140000,
-        diff: 1000
+        diff: 1000,
+        name: '아르카나'
     },
     {
         default: 160000,
-        diff: 1000
+        diff: 1000,
+        name: '모라스'
     },
     {
         default: 180000,
-        diff: 1000
+        diff: 1000,
+        name: '에스페라'
     },
     {
         default: 1320000,
-        diff: -60000
+        diff: -60000,
+        name: '세르니움'
     },
     {
         default: 1500000,
-        diff: -60000
+        diff: -60000,
+        name: '아르크스'
     },
     {
         default: 1680000,
-        diff: -60000
+        diff: -60000,
+        name: '오디움'
+    },
+    {
+        default: 1860000,
+        diff: -60000,
+        name: '도원경'
     }
 ];
+const arcaneCount = 6;
+const authenticCount = 4;
+const arcaneMaxLev = 20;
+const authenticMaxLev = 11;
 
 export const type = '메이플';
 export const commandData = {
@@ -49,7 +67,7 @@ export const commandData = {
             type: ApplicationCommandOptionType.Integer,
             description: '심볼 강화 정보를 계산할 시작 레벨',
             min_value: 1,
-            max_value: 20,
+            max_value: arcaneMaxLev,
             required: true
         },
         {
@@ -57,7 +75,7 @@ export const commandData = {
             type: ApplicationCommandOptionType.Integer,
             description: '심볼 강화 정보를 계산할 목표 레벨',
             min_value: 1,
-            max_value: 20,
+            max_value: arcaneMaxLev,
             required: true
         }
     ]
@@ -70,37 +88,37 @@ export async function commandExecute(interaction) {
     }
 
     const totalReq = [0, 0];
-    const totalMeso = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const totalMeso = Array(arcaneCount + authenticCount).fill(0);
     for (let i = startLev; i < endLev; i++) {
         const arcaneReq = i * i + 11; // 요구량 = i^2 + 11
         totalReq[0] += arcaneReq;
-        for (let j = 0, req = arcaneReq, div = 10000; j < 6; j++) {
-            totalMeso[j] += Math.floor((req * (costConstant[j].default + i * costConstant[j].diff)) / div) * div;
+        for (let j = 0, req = arcaneReq, div = 10000; j < arcaneCount; j++) {
+            totalMeso[j] += Math.floor((req * (symbolConstant[j].default + i * symbolConstant[j].diff)) / div) * div;
         }
 
-        if (i < 11) {
+        if (i < authenticMaxLev) {
             const authenticReq = 9 * i * i + 20 * i; // 요구량 = 9i^2 + 20i
             totalReq[1] += authenticReq;
-            for (let j = 6, req = authenticReq, div = 100000; j < 9; j++) {
-                totalMeso[j] += Math.floor((req * (costConstant[j].default + i * costConstant[j].diff)) / div) * div;
+            for (let j = arcaneCount, req = authenticReq, div = 100000; j < arcaneCount + authenticCount; j++) {
+                totalMeso[j] +=
+                    Math.floor((req * (symbolConstant[j].default + i * symbolConstant[j].diff)) / div) * div;
             }
         }
     }
 
-    await interaction.followUp(
-        `아케인 심볼 Lv.${startLev} → Lv.${endLev}
-요구량: ${totalReq[0]}
-여로: ${totalMeso[0].toLocaleString()}메소
-츄츄: ${totalMeso[1].toLocaleString()}메소
-레헬른: ${totalMeso[2].toLocaleString()}메소
-아르카나: ${totalMeso[3].toLocaleString()}메소
-모라스: ${totalMeso[4].toLocaleString()}메소
-에스페라: ${totalMeso[5].toLocaleString()}메소
+    let arcaneResult = `아케인 심볼 Lv.${startLev} → Lv.${endLev}
+요구량: ${totalReq[0]}`;
+    for (let i = 0; i < arcaneCount; i++) {
+        arcaneResult += `\n${symbolConstant[i].name}: ${totalMeso[i].toLocaleString()}메소`;
+    }
+    let authenticResult = `어센틱 심볼 Lv.${Math.min(authenticMaxLev, startLev)} → Lv.${Math.min(
+        authenticMaxLev,
+        endLev
+    )}
+요구량: ${totalReq[1]}`;
+    for (let i = arcaneCount; i < arcaneCount + authenticCount; i++) {
+        authenticResult += `\n${symbolConstant[i].name}: ${totalMeso[i].toLocaleString()}메소`;
+    }
 
-어센틱 심볼 Lv.${Math.min(11, startLev)} → Lv.${Math.min(11, endLev)}
-요구량: ${totalReq[1]}
-세르니움: ${totalMeso[6].toLocaleString()}메소
-아르크스: ${totalMeso[7].toLocaleString()}메소
-오디움: ${totalMeso[8].toLocaleString()}메소`
-    );
+    await interaction.followUp(`${arcaneResult}\n\n${authenticResult}`);
 }
