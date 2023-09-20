8e0d0169930103b7b3e06f0230789329e03d0550
cjh980402 <9804cjh@naver.com>
2023-09-20 23:03:02 +0900

    썬데이 기능 png 형식 대응, 복수 이미지 대응

--- commands/maple_sunday.js
+++ commands/maple_sunday.js
@@ -22,10 +22,11 @@ export async function commandExecute(interaction) {
     const sunday$ = load(await sundayBody.text());
     const sundayData = sunday$('.contents_wrap');
     const sundayDate = sundayData.find('.event_date').text();
-    const sundayImg = sundayData
+    const sundayImgs = sundayData
         .find('.qs_text .new_board_con div div img')
-        .filter((_, v) => sunday$(v).attr('src').endsWith('.jpg'))
-        .attr('src');
+        .filter((_, v) => /\.(png|jpg)$/.test(sunday$(v).attr('src')))
+        .get()
+        .map((v) => v.attribs.src);
 
-    await interaction.followUp({ content: sundayDate, files: [sundayImg] });
+    await interaction.followUp({ content: sundayDate, files: sundayImgs });
 }
