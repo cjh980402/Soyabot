b7504a36c524cbf2d768a5a97a945dd8b662a339
cjh980402 <9804cjh@naver.com>
2024-06-18 00:22:24 +0900

    한강 기능 status code 체크 추가

--- commands/others_hangang.js
+++ commands/others_hangang.js
@@ -12,24 +12,28 @@ export async function commandExecute(interaction) {
     params.set('currentPage', 1);
     params.set('siteId', '노량진_자동');
 
-    const { body } = await request('https://swo.seoul.go.kr/water/waterMesntkInfoResult.do', {
+    const { statusCode, body } = await request('https://swo.seoul.go.kr/water/waterMesntkInfoResult.do', {
         method: 'POST',
         headers: {
             'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
         },
         body: params.toString()
     });
-    const data = await body.json();
 
-    const result = data.resultList.find((v) => v.W_TEMP);
-    if (!result) {
-        await interaction.followUp('측정된 한강 수온 데이터가 없습니다.');
+    if (200 <= statusCode && statusCode <= 299) {
+        const data = await body.json();
+        const result = data.resultList.find((v) => v.W_TEMP);
+        if (!result) {
+            await interaction.followUp('측정된 한강 수온 데이터가 없습니다.');
+        } else {
+            await interaction.followUp(
+                `한강 수온: ${result.W_TEMP}°C\n업데이트 시간: ${result.MSR_DATE.replace(
+                    /(\d{4})(\d{2})(\d{2})/,
+                    '$1년 $2월 $3일'
+                )} ${result.MSR_TIME.split(':')[0]}시`
+            );
+        }
     } else {
-        await interaction.followUp(
-            `한강 수온: ${result.W_TEMP}°C\n업데이트 시간: ${result.MSR_DATE.replace(
-                /(\d{4})(\d{2})(\d{2})/,
-                '$1년 $2월 $3일'
-            )} ${result.MSR_TIME.split(':')[0]}시`
-        );
+        await interaction.followUp('측정된 한강 수온 데이터가 없습니다.');
     }
 }
