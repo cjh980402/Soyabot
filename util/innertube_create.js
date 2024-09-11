33acb0951b6c716c546b2d1d4a1c607505d13ca4
슈리튬 <9804cjh@naver.com>
2024-09-11 11:15:35 +0900

    새로운 potoken 생성방법 임시코드

--- util/innertube_create.js
+++ util/innertube_create.js
@@ -1,5 +1,7 @@
 import { fetch } from 'undici';
 import { Innertube, Utils, Log } from 'youtubei.js';
+import { JSDOM } from 'jsdom';
+import { BG } from 'bgutils-js';
 import { setTimeout } from 'node:timers/promises';
 import { exec } from '../admin/admin_function.js';
 
@@ -14,13 +16,54 @@ export async function refreshInnertube() {
 }
 
 async function getYoutubePoToken() {
-    const { stdout: data } = await exec('python3 ./util/python/youtube-trusted-session-generator.py', {
+    /*const { stdout: data } = await exec('python3 ./util/python/youtube-trusted-session-generator.py', {
         encoding: 'utf-8'
     }); // 파이썬 스크립트 실행
     const parse = /visitor_data: (.+?)\npo_token: (.+?)\n/.exec(data);
     const token = {
         visitor_data: parse?.[1] ?? '',
         po_token: parse?.[2] ?? ''
+    };*/
+    const innertube = await Innertube.create({ retrieve_player: false });
+    const requestKey = 'O43z0dpjhgX20SCx4KAo';
+    const visitorData = innertube.session.context.client.visitorData;
+
+    const dom = new JSDOM();
+
+    Object.assign(globalThis, {
+      window: dom.window,
+      document: dom.window.document
+    });
+    
+    const bgConfig = {
+      fetch: (url, options) => fetch(url, options),
+      globalObj: globalThis,
+      identifier: visitorData,
+      requestKey,
+    };
+    
+    const challenge = await BG.Challenge.create(bgConfig);
+    
+    if (!challenge)
+      throw new Error('Could not get challenge');
+    
+    if (challenge.script) {
+      const script = challenge.script.find((sc) => sc !== null);
+      if (script)
+        new Function(script)();
+    } else {
+      console.warn('Unable to load Botguard.');
+    }
+    
+    const poToken = await BG.PoToken.generate({
+      program: challenge.challenge,
+      globalName: challenge.globalName,
+      bgConfig
+    });
+
+    const token = {
+        visitor_data: visitorData,
+        po_token: poToken
     };
     console.log(token);
 
